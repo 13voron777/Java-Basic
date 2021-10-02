6b4691ada585c7cd4145f1e049176835a5ecbc83
Oleg <olvor196@gmail.com>
2021-10-02 12:25:57 +0300

    Commit LAB1 HW1 Correct packages in classes and add Correct Lab1

--- src/Lesson_1/BASIC_LAB1/Run.java
+++ src/Lesson_1/BASIC_LAB1/Run.java
@@ -3,17 +3,17 @@ package Lesson_1.BASIC_LAB1;
 public class Run {
     public static void main(String[] args) {
         Address address = new Address();
-        address.country = "Poland";
-        address.city = "Warszawa";
-        address.street = "Wilenska";
-        address.index = "00-000";
-        address.house = 37;
-        address.apartment = 17;
-        System.out.println("Country: " + address.country);
-        System.out.println("City: " + address.city);
-        System.out.println("Street: " + address.street);
-        System.out.println("Index: " + address.index);
-        System.out.println("House: " + address.house);
-        System.out.println("Apartment: " + address.apartment);
+        address.setCountry("Poland");
+        address.setCity("Warszawa");
+        address.setStreet("Wilenska");
+        address.setIndex("00-000");
+        address.setHouse(37);
+        address.setApartment(17);
+        System.out.println("Country: " + address.getCountry());
+        System.out.println("City: " + address.getCity());
+        System.out.println("Street: " + address.getStreet());
+        System.out.println("Index: " + address.getIndex());
+        System.out.println("House: " + address.getHouse());
+        System.out.println("Apartment: " + address.getApartment());
     }
 }
