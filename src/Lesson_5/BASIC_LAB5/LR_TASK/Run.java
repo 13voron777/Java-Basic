3b2720743cb039183c308b4be2334263dac82c3e
Oleg <olvor196@gmail.com>
2021-10-12 20:54:12 +0300

    commit 5 LR correcting

--- src/Lesson_5/BASIC_LAB5/LR_TASK/Run.java
+++ src/Lesson_5/BASIC_LAB5/LR_TASK/Run.java
@@ -19,12 +19,30 @@ public class Run {
 
 
         while (!bool) {
+            System.out.print("Введите число от 1 до 5 (-1 для выхода): ");
             switch (scanner.nextInt()) {
                 case 1:
                     list.add(scanner.nextInt());
                     break;
                 case 2:
-                    list.remove(list.size() - 1);
+                    boolean bool2 = false;
+                    while (!bool2) {
+                        System.out.print("Введите 0 для удаления по индексу и " +
+                                "1 для удаления по объекту: ");
+                        int choose = scanner.nextInt();
+                        switch (choose) {
+                            case 0:
+                                list.remove(scanner.nextInt());
+                                bool2 = true;
+                                break;
+                            case 1:
+                                list.remove((Integer) scanner.nextInt());
+                                bool2 = true;
+                                break;
+                            default:
+                                System.out.println("Можно ввести только 0 или 1");
+                        }
+                    }
                     break;
                 case 3:
                     for (Integer num : list) {
@@ -40,7 +58,17 @@ public class Run {
                     }
                     break;
                 case 5:
-                    list.set(scanner.nextInt(), scanner.nextInt());
+                    boolean bool5 = false;
+                    while (!bool5) {
+                        System.out.print("Введите индекс элемента для замены: ");
+                        int index = scanner.nextInt();
+                        if (index >= 0 && index <= list.size() - 1) {
+                            list.set(index, scanner.nextInt());
+                            bool5 = true;
+                        } else {
+                            System.out.println("Вы вышли за рамки списка, введите число еще раз!");
+                        }
+                    }
                     break;
                 case -1:
                     bool = true;
