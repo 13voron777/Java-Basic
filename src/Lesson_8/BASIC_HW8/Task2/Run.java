e3142e505c932c1452567219d2692989c5eb1f7e
Oleg <olvor196@gmail.com>
2021-10-14 02:24:05 +0300

    commit 8 hw with sort

--- src/Lesson_8/BASIC_HW8/Task2/Run.java
+++ src/Lesson_8/BASIC_HW8/Task2/Run.java
@@ -22,13 +22,42 @@ public class Run {
                     workers[i].setStartYear(year);
                     bool = true;
                 } catch (Exception e) {
-                    System.out.println("Значение года введено неверно");
+                    System.out.println("Значение года введено неверно!\n");
                 }
             }
         }
 
+        for (int i = 0; i < workers.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (workers[i].getFio().compareTo(workers[j].getFio()) < 0) {
+                    Worker tempWorker = workers[i];
+                    workers[i] = workers[j];
+                    workers[j] = tempWorker;
+                }
+                if (workers[i].getFio().equals(workers[j].getFio())) {
+                    if (workers[i].getWork().compareTo(workers[i - 1].getWork()) < 0) {
+                        Worker tempWorker = workers[i];
+                        workers[i] = workers[j];
+                        workers[j] = tempWorker;
+                    }
+                }
+            }
+        }
+        System.out.println();
+
+        System.out.println("Весь список, отсортированный по алфавиту:");
         for (Worker worker : workers) {
             System.out.println(worker);
+        }
+        System.out.println();
+
+        System.out.print("Введите стаж: ");
+        int exp = scanner.nextInt();
+        System.out.println("Список работников, стаж которых превышает " + exp + " лет:");
+        for (Worker worker : workers) {
+            if (2021 - worker.getStartYear() > exp) {
+                System.out.println(worker);
+            }
         }
     }
 }
--- src/Lesson_8/BASIC_HW8/Task3/Run.java
+++ src/Lesson_8/BASIC_HW8/Task3/Run.java
@@ -14,13 +14,34 @@ public class Run {
             prices[i].setCost(scanner.nextDouble());
         }
 
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (prices[i].getMarket().compareTo(prices[j].getMarket()) < 0) {
+                    Price tempPrice = prices[i];
+                    prices[i] = prices[j];
+                    prices[j] = tempPrice;
+                }
+            }
+        }
+        System.out.println();
+
+        System.out.println("Весь список, отсортированный по алфавиту:");
+        for (Price price : prices) {
+            System.out.println(price);
+        }
+        System.out.println();
+
         boolean bool = false;
         while (!bool) {
             try {
+                System.out.print("Введите название магазина: ");
                 String market = scanner.next();
                 int count = 0;
                 for (Price price : prices) {
                     if (price.getMarket().equals(market)) {
+                        if (count == 0) {
+                            System.out.println("\nИнформация о товарах указанного магазина:");
+                        }
                         System.out.println(price);
                         count++;
                     }
@@ -31,7 +52,7 @@ public class Run {
                     bool = true;
                 }
             } catch (Exception e) {
-                System.out.println("Список не содержит введенного магазина!");
+                System.out.println("Список не содержит введенного магазина\n");
             }
         }
     }
