e5f716ece11ce69b39a9af777d2a76f5ea290a89
Oleg <olvor196@gmail.com>
2021-10-15 04:43:29 +0300

    commit 9 Extra

--- src/Lesson_9/BASIC_LAB9/Extra/Animal.java
+++ src/Lesson_9/BASIC_LAB9/Extra/Animal.java
@@ -1,7 +1,5 @@
 package Lesson_9.BASIC_LAB9.Extra;
 
-import java.util.Objects;
-
 public class Animal {
     private String name;
     private int age;
@@ -37,11 +35,15 @@ public class Animal {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Animal animal = (Animal) o;
-        return age == animal.age && tail == animal.tail && Objects.equals(name, animal.name);
+        return age == animal.age && tail == animal.tail && name.equals(animal.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, age, tail);
+        int result = 17;
+        result = 37 * result + (name == null ? 0 : name.hashCode());
+        result = 37 * result + age;
+        result = 37 * result + (tail ? 1 : 0);
+        return result;
     }
 }
--- src/Lesson_9/BASIC_LAB9/Extra/Run.java
+++ src/Lesson_9/BASIC_LAB9/Extra/Run.java
@@ -2,7 +2,27 @@ package Lesson_9.BASIC_LAB9.Extra;
 
 public class Run {
     public static void main(String[] args) {
-        Animal animal = new Animal("Васька", 45, false);
-        System.out.println(animal);
+        Animal animal1 = new Animal("Васька", 45, false);
+        Animal animal2 = new Animal("Васька", 45, false);
+        Animal animal3 = animal1;
+
+        System.out.println(animal1);
+        System.out.println(animal2);
+        System.out.println(animal3);
+        System.out.println();
+
+        System.out.println(animal1.hashCode());
+        System.out.println(animal2.hashCode());
+        System.out.println(animal3.hashCode());
+        System.out.println();
+
+        System.out.println(animal1.equals(animal2));
+        System.out.println(animal1.equals(animal3));
+        System.out.println(animal2.equals(animal3));
+        System.out.println();
+
+        System.out.println(animal1 == animal2);
+        System.out.println(animal1 == animal3);
+        System.out.println(animal2 == animal3);
     }
 }
