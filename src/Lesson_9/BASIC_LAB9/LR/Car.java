2e7d6076f72a5ee64dd7e816581ea63c847d9299
Oleg <olvor196@gmail.com>
2021-10-23 12:50:06 +0300

    commit 9 lab

---
+++ src/Lesson_9/BASIC_LAB9/LR/Car.java
@@ -0,0 +1,113 @@
+package Lesson_9.BASIC_LAB9.LR;
+
+public class Car {
+    private int id;
+    private String name;
+    private String model;
+    private int year;
+    private String color;
+    private double price;
+    private String regNumber;
+
+    public Car(int id, String name, String model, int year, String color, double price, String regNumber) {
+        this.id = id;
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.regNumber = regNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{ " +
+                "id = " + id +
+                ", name = " + name +
+                ", model =" + model +
+                ", year = " + year +
+                ", color = " + color +
+                ", price = " + price +
+                ", regNumber = " + regNumber +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id && year == car.year && price == car.price
+                && name.equals(car.name) && model.equals(car.model)
+                && color.equals(car.color) && regNumber.equals(car.regNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + id;
+        result = 37 * result + (name == null ? 0 : name.hashCode());
+        result = 37 * result + (model == null ? 0 : model.hashCode());
+        result = 37 * result + year;
+        result = 37 * result + (color == null ? 0 : color.hashCode());
+        result = (int) (37 * result + Double.doubleToLongBits(price));
+        result = 37 * result + (regNumber == null ? 0 : regNumber.hashCode());
+        return result;
+    }
+}
---
+++ src/Lesson_9/BASIC_LAB9/LR/Run.java
@@ -0,0 +1,72 @@
+package Lesson_9.BASIC_LAB9.LR;
+
+import java.util.Scanner;
+
+public class Run {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Car[] cars = new Car[7];
+        cars[0] = new Car(0, "BMW", "320d", 2013, "Black", 40000, "ABCD");
+        cars[1] = new Car(1, "BMW", "X5", 2011, "Blue", 60000, "ABCDEF");
+        cars[2] = new Car(2, "Mercedes", "Gelentwagen", 2011, "Black", 80000, "ABCDNM");
+        cars[3] = new Car(0, "BMW", "320d", 2013, "Black", 40000, "ABCD");
+        cars[4] = new Car(0, "BMW", "320d", 2013, "Black", 40000, "ABCD");
+        cars[5] = new Car(3, "Wolkswagen", "Passat", 2008, "Black", 30000, "ABKL");
+        cars[6] = new Car(4, "Mazda", "CX-5", 2008, "Black", 40000, "AIOF");
+
+        while (true) {
+            System.out.println("Меню");
+            System.out.println("Введите 1 для вывода списка марок автомобилей из массива cars");
+            System.out.println("Введите 2 для вывода списка автомобилей, которым больше n лет");
+            System.out.println("Введите 3 для вывода списка автомобилей заданного года выпуска, " +
+                    "цена которых больше указанной");
+            System.out.println("Введите 4 для вывода списка одинаковых автомобилей в сравнении с первым" +
+                    "в массиве cars");
+            System.out.println("Введите 0 для выхода из программы");
+
+            String userIn = scanner.next();
+            if (userIn.equals("0")) {
+                break;
+            } else {
+                switch (userIn) {
+                    case "1":
+                        for (Car car : cars) {
+                            System.out.println(car.getModel());
+                        }
+                        break;
+                    case "2":
+                        System.out.print("Введите n: ");
+                        int n = scanner.nextInt();
+                        for (Car car : cars) {
+                            if (2021 - car.getYear() > n) {
+                                System.out.println(car);
+                            }
+                        }
+                        break;
+                    case "3":
+                        System.out.println("Введите год выпуска: ");
+                        int year = scanner.nextInt();
+                        System.out.println("Введите цену: ");
+                        double price = scanner.nextDouble();
+                        for (Car car : cars) {
+                            if (car.getYear() == year && car.getPrice() > price) {
+                                System.out.println(car);
+                            }
+                        }
+                        break;
+                    case "4":
+                        int count = 0;
+                        for (int i = 1; i < cars.length; i++) {
+                            if (cars[i].equals(cars[0]) && cars[i].hashCode() == cars[0].hashCode()) {
+                                count++;
+                                System.out.println(cars[i]);
+                            }
+                        }
+                        System.out.println("Количество одинаковых авто в сравнении с первым в массиве cars: " + count);
+                        break;
+                }
+            }
+        }
+    }
+}
